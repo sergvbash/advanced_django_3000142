c8d565600e6f3349466fd8ca3a5ce534aa65a6d3
sergvbash <bask.sv@gmail.com>
2022-10-24 13:10:26 -0400

    1

--- 01_03/friendsbook/post/models.py
+++ 01_03/friendsbook/post/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
+from django.core.exceptions import ValidationError
 
+
+BAD_WORDS = ['java','waterfall']
+
+def validade_no_bad_words(content):
+    if any([word in content.lower() for word in BAD_WORDS]):
+        raise ValidationError('This post contain bad words')
 
 class Post(models.Model):
     author = models.ForeignKey(User, null=True, blank=True, on_delete=CASCADE)
-    content = models.CharField(max_length=140)
+    content = models.CharField(max_length=140, validators=[validade_no_bad_words])
     created_on = models.DateTimeField('date created', auto_now_add=True)
 
     def __str__(self):
--- 01_03/friendsbook/profilepage/models.py
+++ 01_03/friendsbook/profilepage/models.py
@@ -4,12 +4,29 @@ from django.apps import apps
 from useraccount.models import UserAccount
 from business.models import Business
 
+
+
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class ProfilePage(models.Model):
-    user_account = models.ForeignKey(UserAccount, null=True, blank=False, on_delete=CASCADE)
+    # user_account = models.ForeignKey(UserAccount, null=True, blank=False, on_delete=CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    subject = GenericForeignKey('content_type','object_id')
 
-    def __str__(self):
-        return '{}'.format(self.user_account.first_name)
+    # def __str__(self):
+    #     return '{}'.format(self.user_account.first_name)
+
+@receiver(post_save, sender=UserAccount)
+def create_profile_user(instance, created, **kwargs):
+    if created:
+        ProfilePage.objects.create(subject=instance)
+
+@receiver(post_save, sender=Business)
+def create_profile_business(instance, created, **kwargs):
+    if created:
+        ProfilePage.objects.create(subject=instance)
